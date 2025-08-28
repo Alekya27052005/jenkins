81b13c610a5a4a74a4271e61c77358f108b1e10b
Alekya27052005 <alekyareddy2705@gmail.com>
2025-08-28 21:34:48 +0530

    adding samplename.java

---
+++ samplename.java
@@ -0,0 +1,7 @@
+public class SampleName {
+    public static void main(String[] args) {
+        String name = "Sreethana";
+
+// Print the name to the console
+
+System.out.println("Hello, my name is " + name);
